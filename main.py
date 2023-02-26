8a9b809fd680f6f9be2e08c1964d303ed4b5facf
Ryan M Howell <rmhowell@protonmail.com>
2023-02-26 15:15:45 -0800

    :children_crossing: Make interactive.

--- main.py
+++ main.py
@@ -4,13 +4,19 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def run():
+    # Get the degenerate gambler's name.
+    name = input('Enter your name: ')
+
+    # Welcome them to the roulette wheel simulator.
+    print(f'Welcome to the roulette wheel simulator, {name}')
+
+    # Exit
+    print("Have a good day!")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
